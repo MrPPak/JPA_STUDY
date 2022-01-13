89ecb66d3a23a819f4212f7db1a2abce686a0c94
MrPPak <matht1115@naver.com>
2022-01-13 09:24:09 +0900

    init jpa-basic-test project

---
+++ jpa-basic-test/src/main/java/com/jpa/basic/test/JpaBasicTestApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.basic.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicTestApplication.class, args);
+	}
+
+}
