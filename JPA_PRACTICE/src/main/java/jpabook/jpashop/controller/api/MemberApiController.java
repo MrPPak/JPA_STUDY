92584fee27cafeeaeac4b7e85e1f7c45355c1ac0
MrPPak <matht1115@naver.com>
2022-02-27 22:51:18 +0900

    MemberApiController 전부 구현

--- JPA_PRACTICE/src/main/java/jpabook/jpashop/controller/api/MemberApiController.java
+++ JPA_PRACTICE/src/main/java/jpabook/jpashop/controller/api/MemberApiController.java
@@ -5,90 +5,48 @@ import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Update;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
+   private final MemberService memberService;
 
-    private final MemberService memberService;
+   @PostMapping("/api/v1/members")
+   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+       Long id = memberService.join(member);
+       return new CreateMemberResponse(id);
+   }
 
-    @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
-    }
+   @PostMapping("/api/v2/members")
+   public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+       Member member = request.toEntity();
+       Long id = memberService.join(member);
 
-    @PostMapping("/api/v2/members")
-    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
-        Member member = new Member();
-        member.setName(request.getName());
+       return new CreateMemberResponse(id);
+   }
 
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
-    }
+   @Data
+   static class CreateMemberRequest {
+       private String name;
 
-    @PatchMapping("/api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
-        memberService.update(id, request.getName());
-        Member findMember = memberService.findOne(id);
-        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
-    }
+       public Member toEntity() {
+           Member member = new Member();
+           member.setName(this.name);
 
-    @GetMapping("/api/v1/members")
-    public List<Member> membersV1() {
-        return memberService.findMembers();
-    }
+           return member;
+       }
+   }
 
-    @GetMapping("/api/v2/members")
-    public Result<List<MemberDto>> membersV2() {
+   @Data
+   @AllArgsConstructor
+   static class CreateMemberResponse {
+       private Long id;
+   }
 
-        List<Member> findMembers = memberService.findMembers();
 
-        // 엔티티 -> DTO 변환
-        List<MemberDto> collect = findMembers.stream().map(member -> new MemberDto(member.getName())).collect(Collectors.toList());
-
-        return new Result<>(collect, collect.size());
-    }
-
-    @Data
-    static class CreateMemberRequest {
-        private String name;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class CreateMemberResponse {
-        private Long id;
-    }
-
-    @Data
-    static class UpdateMemberRequest {
-        private String name;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class UpdateMemberResponse {
-        private Long id;
-        private String name;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class Result<T> {
-        private T data;
-        private int count;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class MemberDto {
-        private String name;
-    }
 }
